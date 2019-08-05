d99b25d047bc448fef4a7b258b85b8cb73def905
moonseoklee <35364566+moonseoklee@users.noreply.github.com>
2019-08-05 15:01:33 +0900

    remove secretK

--- kugrade/settings.py
+++ kugrade/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'l3k02j#1g+o3eu!2^6tn3a5rg-1*=^tr&mnpo(tc&4sd3xrjv&'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
